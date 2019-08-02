6166895c6721c96b27da71586f714101e256b56f
Lawrence Fiesta <lfiesta86@gmail.com>
2019-08-02 09:09:58 -0700

    Updates

---
+++ precache-manifest.b64aa62d39af28df5002285f67e80967.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b5a9a5320c462229237ebdebec7a0a",
+    "url": "/clingy-react/index.html"
+  },
+  {
+    "revision": "941a0559e74e818846da",
+    "url": "/clingy-react/static/css/main.7d69e242.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clingy-react/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "941a0559e74e818846da",
+    "url": "/clingy-react/static/js/main.90b8f66c.chunk.js"
+  },
+  {
+    "revision": "a2323d91f100ba7650aa",
+    "url": "/clingy-react/static/js/runtime~main.c1bb17bd.js"
+  }
+]);
